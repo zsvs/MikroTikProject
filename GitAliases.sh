8634eaa6e354638dd2476c27f65d5a08d47ce406
ZSVS <stepanezc@gmail.com>
2021-07-09 14:45:13 +0300

    Init commit

---
+++ GitAliases.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+alias gss="git status"
+alias ga="git add"
+alias gaa="git add ."
+alias gb="git branch"
+alias gc="git commit -m"
+alias gd="git diff"
+alias gco="git checkout"
+alias gp="git push"
---
+++ roles/MikroTikConf/README.md
@@ -0,0 +1,13 @@
+Role Name
+=========
+
+Role for configuring MikroTik RouterOS via ssh
+
+Requirements
+------------
+Use pip install requirements.txt for installing ansible dependencies 
+
+License
+-------
+
+BSD
